e94528849fa7b1fe2b523eeb8c25d8d8238597e5
Dustin Taylor <vulfox91@gmail.com>
2023-10-17 11:51:06 -0400

    Update to latest Mach/Zig

--- build.zig
+++ build.zig
@@ -1,17 +1,12 @@
 const std = @import("std");
 
 const mach = @import("mach");
-const mach_freetype = @import("mach_freetype");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const mach_dep = b.dependency("mach", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const mach_freetype_dep = b.dependency("mach_freetype", .{
         .target = target,
         .optimize = optimize,
     });
@@ -22,13 +17,9 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
         .mach_builder = mach_dep.builder,
-        .deps = &[_]std.build.ModuleDependency{
-            .{ .name = "mach-freetype", .module = mach_freetype_dep.module("mach-freetype") },
-        },
+        .deps = &[_]std.build.ModuleDependency{},
     });
     if (b.args) |args| app.run.addArgs(args);
-
-    mach_freetype.linkFreetype(mach_freetype_dep.builder, app.compile);
 
     // Test Step
     const run_step = b.step("run", "Run the app");
